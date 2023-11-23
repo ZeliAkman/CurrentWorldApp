4dc92068945a144a9760a0018f3d9814577eacce
zeli <akmanzuley90@gmail.com>
2023-11-24 00:38:13 +0300

    Design Page for CurrentWorld App

---
+++ app/src/main/java/com/example/cuurentworld/NewsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cuurentworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+
+        //Your API key is: db2611256dd545b89d34e3f2bcd420fb
+    }
+}
